30865d864eac736b24c7ed9d056f6852c5052648
Wallace Derricotte <wallace@master.cl.he>
2015-07-29 10:49:23 -0400

    Added a try and except block so that if the SCF fails, the algorithm will reuse previous coordinates and try again

--- psi_metropolis.py
+++ psi_metropolis.py
@@ -68,37 +68,44 @@ def monte_carlo(mol, method, temperature, max_step, total_mc_moves):
 		# Activate the Molecule object created from the new coordinates
 		# and run the QM method on it
 		psi4.set_active_molecule(new_mol)
-		E2 = psi4.scf()
+		try:
+			E2 = psi4.scf() 
 
-		mc_out.write("%s energy with old geometry: %f\n" %(method,E1))
-		mc_out.write("%s energy with new geometry: %f\n" %(method,E2))
+			mc_out.write("%s energy with old geometry: %f\n" %(method,E1))
+			mc_out.write("%s energy with new geometry: %f\n" %(method,E2))
 
-		Energy_Difference = E2 - E1
-		mc_out.write("energy difference: %f\n" %(Energy_Difference))
-		Energy_Difference_eV = Energy_Difference*hartree2eV
-		#mol = acceptance_check(Energy_Difference,temperature, new_mol, old_mol, natoms, mc_iteration)  
-		Boltz = np.exp(- Energy_Difference/(8.617e-5 * temperature))
-		R = rand.random()
-		trajectory_file = open("trajectory.xyz" , "a")
-		if (abs(Energy_Difference) >= 1e-10):
-			mc_out.write("\n Significant Change in Energy Performing acceptance check (R = %f) \n" %R)
-			if (Boltz > R):
-				mc_out.write("Coordinates Accepted: Using New Coordinates for Next Monte Carlo Run \n")
-				mol = new_mol
-				trajectory_file.write("%d \n" %natoms)
-				trajectory_file.write("Step %d of Psi4 Monte-Carlo Simulation (Coordinates Updated) \n" %(mc_iteration))
-				for i in range(natoms):
-					trajectory_file.write("%s  " %labels[i])
-					for j in range(3):
-						if (j == 2):
-							trajectory_file.write("    %f \n" %(New_Geometry[i][j]))
-						else:
-							trajectory_file.write("    %f" %(New_Geometry[i][j]))
+			Energy_Difference = E2 - E1
+			mc_out.write("energy difference: %f\n" %(Energy_Difference))
+			Energy_Difference_eV = Energy_Difference*hartree2eV
+			#mol = acceptance_check(Energy_Difference,temperature, new_mol, old_mol, natoms, mc_iteration)  
+			Boltz = np.exp(- Energy_Difference/(8.617e-5 * temperature))
+			R = rand.random()
+			trajectory_file = open("trajectory.xyz" , "a")
+			if (abs(Energy_Difference) >= 1e-10):
+				mc_out.write("\n Significant Change in Energy Performing acceptance check (R = %f) \n" %R)
+				if (Boltz > R):
+					mc_out.write("Coordinates Accepted: Using New Coordinates for Next Monte Carlo Run \n")
+					mol = new_mol
+					trajectory_file.write("%d \n" %natoms)
+					trajectory_file.write("Step %d of Psi4 Monte-Carlo Simulation (Coordinates Updated) \n" %(mc_iteration))
+					for i in range(natoms):
+						trajectory_file.write("%s  " %labels[i])
+						for j in range(3):
+							if (j == 2):
+								trajectory_file.write("    %f \n" %(New_Geometry[i][j]))
+							else:
+								trajectory_file.write("    %f" %(New_Geometry[i][j]))
+				else:
+					mc_out.write("Coordinates Denied: Repeating Monte Carlo Run With Coordinates From Previous Run \n")
+					mol = old_mol
 			else:
-				mc_out.write("Coordinates Denied: Repeating Monte Carlo Run With Coordinates From Previous Run \n")
+				mc_out.write("No Change in Energy: Repeating Monte Carlo Run With Old Coordinates \n")
 				mol = old_mol
-		else:
-			mc_out.write("No Change in Energy: Repeating Monte Carlo Run With Old Coordinates \n")
-			mol = old_mol
-		trajectory_file.close()
-		mc_out.close()
+			trajectory_file.close()
+			mc_out.close()
+		
+		except RuntimeError:
+			mc_out.write("SCF Did Not Converge: Repeating Monte Carlo Run With Old Coordinates \n")
+                        mol = old_mol
+                        trajectory_file.close()
+                        mc_out.close()
